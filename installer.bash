27003d520e60cb2c05ce02c19f9f0f527a09c296
Tuhil <Tuhil>
2025-07-30 15:29:34 +0530

    made it so the installer doesn't replace previous configs

--- installer.bash
+++ installer.bash
@@ -12,8 +12,8 @@ cp reminder_app_settings.py ~/.config/reminder_app
 chmod +x ~/.config/reminder_app/reminder_app_settings.py
 cp reminder_sound.wav ~/.config/reminder_app
 cp reminder_app_logs.txt ~/.config/reminder_app
-cp reminders.txt ~/.config/reminder_app
-cp courses.txt ~/.config/reminder_app
+cp -n reminders.txt ~/.config/reminder_app
+cp -n courses.txt ~/.config/reminder_app
 chmod +x reminder_app_startup.sh
 sudo cp reminder_app_startup.sh /etc/profile.d
 chmod +x reminder_app
